4269196f37dcd33211727f65810315bedecb413d
Harrykumawatt <118455462+Harrykumawatt@users.noreply.github.com>
2022-11-30 05:14:13 -0800

    Day3 completed

--- lib/Login_page.dart
+++ lib/Login_page.dart
@@ -6,12 +6,34 @@ class Login_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "i am Harry",
-          style: TextStyle(
-              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blue),
-        ),
+      child: Column(
+        children: [
+          Image.network(
+            "https://duet-cdn.vox-cdn.com/thumbor/0x0:2012x1341/750x500/filters:focal(1006x670:1007x671):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/15483559/google2.0.0.1441125613.jpg",
+            fit: BoxFit.cover,
+          ),
+          Text(
+            "WelCome",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter your name ", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter your Password ", labelText: "Password"),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Home_page.dart';
 import 'package:flutter_application_1/Login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,14 +13,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.dark,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(brightness: Brightness.light),
-      initialRoute: "/home",
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/",
       routes: {
         "/": (context) => Login_page(),
         "/home": (context) => Home_page(),
-        "/login": ((context) => Login_page())
+        "/login": (context) => Login_page(),
       },
     );
   }
